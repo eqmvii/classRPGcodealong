024ef5a5fd8359d22ab4cda52a1f7a3d7259e3f4
Eric (Martin) Mancini <coelomate@gmail.com>
2018-04-07 10:58:16 -0400

    constructor work

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 console.log("Booting up RPG exmaple!");
 
-function Character(name){
+function Pokemon(name, level = 0) {
+    // Stats
     this.name = name;
+    this.level = level;
+    this.hp = 100;
+    this.attack = 10;
+    this.defense = 10;
+    this.special = 10;
+
+    // Methods
     this.greet = function() {
         console.log(`Oh hello! My name is ${this.name}.`);
-    }
+    };
+    this.printStats = function(){
+        console.log(`${this.name}'s stats:`);
+        console.log(`Name: ${this.name} | lvl: ${this.level} | HP: ${this.hp} | att: ${this.attack} | def: ${this.defense} | spcl: ${this.special}`);
+    };
 }
 
-var pikachu = new Character("pikachu");
+var pikachu = new Pokemon("pikachu");
 pikachu.greet();
+pikachu.printStats();
